87ad57b6fec6648ba487ad8aff08f31cdbb54809
RQuirogaH <rafarqh@gmail.com>
2022-04-02 02:39:25 -0500

    Seleccionar color/grosor/boton borrar

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 let objCanva = document.getElementById("spaceCanva");
+let objColor = document.getElementById("colorInput");
+let objLineWidth = document.getElementById("lineWidthInput");
+let objCleanButton = document.getElementById("cleanButtonInput");
 let lienzo = objCanva.getContext("2d");
 let color = "#000";
 let w = objCanva.width;
 let h = objCanva.height;
-document.addEventListener("mousemove", dibujarMov);
-document.addEventListener("mousedown", clickStart);
 var x = 0;
 var y = 0;
+
+document.addEventListener("mousemove", dibujarMov);
+document.addEventListener("mousedown", clickStart);
+objCleanButton.addEventListener("click", cleanCanva)
+
 
 /*
 ----------- Funciones ----------
 */
-function dibujar(xo, yo, xf, yf) {
+function dibujar(xo, yo, xf, yf, color, tamañoLinea) {
     lienzo.beginPath();
     lienzo.strokeStyle = color;
+    lienzo.lineWidth = tamañoLinea;
     lienzo.moveTo(xo, yo);
     lienzo.lineTo(xf, yf);
     lienzo.stroke();
@@ -24,25 +31,22 @@ function clickStart(event) {
     if (event.target.id === "spaceCanva" && event.buttons === 1){
         x = event.offsetX;
         y = event.offsetY;
+        color = objColor.value;
+        lineWidth = objLineWidth.value;
+        return x, y, color, lineWidth;
     }
-    return x, y;
 }
 
-
 function dibujarMov(event) {
+    console.log(event);
     if (event.target.id === "spaceCanva" && event.buttons === 1){
         let xf = event.offsetX;
         let yf = event.offsetY;
-        dibujar(x, y, xf, yf);
+        dibujar(x, y, xf, yf, color, lineWidth);
         return x = xf, y = yf;
     }
 }
 
-/* 
------ Dibujar borde del liezo -------
-*/
-
-dibujar(0, 0, 0, h);
-dibujar(0, h, w, h);
-dibujar(w, h, w, 0);
-dibujar(w, 0, 0, 0);
+function cleanCanva() {
+    lienzo.clearRect(0, 0, w, h);
+}
